f40c0a9ec4bca661007c5c165765f97b8238fa2c
naveen g l <glnaveen06@gmail.com>
2022-10-21 19:25:00 +0530

    21-10-2022

---
+++ Basic_programm2.java
@@ -0,0 +1,16 @@
+class Programm{
+public static void main(String...args){
+System.out.println("Programm Class");
+}
+
+}
+class Programm1{
+public static void main(String...args){
+System.out.println("Programm1 Class");
+}
+
+}
+class Programm2{
+
+
+}
---
+++ First_programm.java
@@ -0,0 +1,8 @@
+class Basic_program{
+
+	public static void main(String...args){
+		System.out.println("This is my First Programm.....");
+}
+
+}
+
